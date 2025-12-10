0e5b400e03b8ada1c7ef93db6ce81782d5f95da5
vladikslon <vladbrovi@gmail.com>
2025-12-10 18:41:22 +0000

    create api service and authentication models

--- app/src/main/java/com/example/safeair/api/ApiService.kt
+++ app/src/main/java/com/example/safeair/api/ApiService.kt
@@ -1,4 +1,12 @@
 package com.example.safeair.api
 
-class ApiService {
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+
+interface ApiServices {
+    @POST("auth/login")
+    suspend fun login(@Body request: AuthModels.LoginRequest): Response<AuthModels.LoginResponse>
+
 }
--- app/src/main/java/com/example/safeair/api/AuthModels.kt
+++ app/src/main/java/com/example/safeair/api/AuthModels.kt
@@ -1,4 +1,15 @@
 package com.example.safeair.api
 
 class AuthModels {
+    data class LoginRequest(
+        val username: String,
+        val password: String
+    )
+
+    data class LoginResponse(
+        val msg: String? = null,
+        val access_token: String? = null,
+
+    )
+
 }
